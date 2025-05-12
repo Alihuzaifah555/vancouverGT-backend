3af0fbe8130f3919cf26cea2665e3d972273b8e7
alihuzaifah12 <alihuzaifah@softpyramid.dev>
2025-05-12 16:13:52 +0500

    updated

--- api/review.js
+++ api/review.js
@@ -2,15 +2,15 @@ const dbConnect = require('../../utils/db');
 const Review = require('../../models/Review');
 module.exports = async function handler(req, res) {
     // if (req.method === 'GET') {
-      try {
+    //   try {
         // await dbConnect(); // connect to DB
         // const reviews = await Review.find().sort({ createdAt: -1 }); // latest on top
         res.status(200).json({
             message: 'Hello from the API!',
         });
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
+    //   } catch (err) {
+    //     res.status(500).json({ error: err.message });
+    //   }
     // } else {
     //   res.status(405).json({ message: 'Method not allowed' });
     // }
