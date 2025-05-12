c8b5804ed49a793b00a0e34d52c11da7edd9a966
alihuzaifah12 <alihuzaifah@softpyramid.dev>
2025-05-12 16:09:48 +0500

    updated

--- api/review.js
+++ api/review.js
@@ -1,7 +1,6 @@
-const dbConnect = require('../../utils/db');
-const Review = require('../../models/Review');
+// const dbConnect = require('../../utils/db');
+// const Review = require('../../models/Review');
 module.exports = async function handler(req, res) {
     res.status(200).json({ message: 'API is working!' });
-
-  }
+}
 
