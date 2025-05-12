faad96b1f5bf7c86a4c55fdc0abcc30413fb7867
alihuzaifah12 <alihuzaifah@softpyramid.dev>
2025-05-12 15:59:08 +0500

    updated

--- api/review.js
+++ api/review.js
@@ -3,9 +3,11 @@ const Review = require('../../models/Review');
 module.exports = async function handler(req, res) {
     if (req.method === 'GET') {
       try {
-        await dbConnect(); // connect to DB
-        const reviews = await Review.find().sort({ createdAt: -1 }); // latest on top
-        res.status(200).json(reviews);
+        // await dbConnect(); // connect to DB
+        // const reviews = await Review.find().sort({ createdAt: -1 }); // latest on top
+        res.status(200).json({
+            message: 'Hello from the API!',
+        });
       } catch (err) {
         res.status(500).json({ error: err.message });
       }
